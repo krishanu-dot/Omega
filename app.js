9e2cd8fc220bad344025121c832a347546eb4612
krishanu-dot <69411336+krishanu-dot@users.noreply.github.com>
2020-08-09 10:19:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyBNvQza1qW-7y9wyE-BTgi2ivGenuE8IUc",
+  authDomain: "hacking-c06ea.firebaseapp.com",
+  databaseURL: "https://hacking-c06ea.firebaseio.com",
+  projectId: "hacking-c06ea",
+  storageBucket: "hacking-c06ea.appspot.com",
+  messagingSenderId: "922813367195",
+  appId: "1:922813367195:web:300a8acb567b2df8b1573e",
+  measurementId: "G-XBJC4S3VPM"
 };
 
      
